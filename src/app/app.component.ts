47e679c3c310fcfa8eabbee8be57da22a7d807dc
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-02-05 19:43:17 +0200

    edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GoogleBooksSerice } from './book-list/books.serice';
+import { BooksActions, BooksApiActions } from './state/books.actions';
+import { selectBookCollection, selectBooks } from './state/books.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NgRxBooklist';
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(private booksService: GoogleBooksSerice, private store: Store) {}
+
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
+
+  ngOnInit(): void {
+    this.booksService
+      .getBooks()
+      .subscribe((books) =>
+        this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+      );
+  }
 }
